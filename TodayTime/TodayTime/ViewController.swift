77bdfb2975b0e895d86c4955fc699f320235f6e7
jungwoo Lee <b5nnnal88@gmail.com>
2021-07-17 23:13:23 +0900

    apply time remaining

--- TodayTime/TodayTime/ViewController.swift
+++ TodayTime/TodayTime/ViewController.swift
@@ -12,33 +12,85 @@ class ViewController: UIViewController {
     var mTimer : Timer?
     let dateFormatter = DateFormatter()
     
-    @IBOutlet weak var nowTime : UILabel!
+    @IBOutlet weak var nowSecond : UILabel!
+    @IBOutlet weak var nowMinute : UILabel!
+    @IBOutlet weak var nowHour   : UILabel!
 
     override func viewDidLoad() {
-        let todayTime = Date()
-        dateFormatter.dateFormat = "HH : mm : ss"
         super.viewDidLoad()
-        nowTime.text = dateFormatter.string(from: todayTime)
+        let todayTime = Date()
+        dateFormatter.dateFormat = "ss"
+        let second = 60 - Int(dateFormatter.string(from: todayTime))!
+        if second < 10 {
+            nowSecond.text = "0"+String(second)
+        }else{
+            nowSecond.text = String(second)
+        }
+        
+        dateFormatter.dateFormat = "mm"
+        let minute = 59 - Int(dateFormatter.string(from: todayTime))!
+        if minute < 10 {
+            nowMinute.text = "0"+String(minute)
+        }else{
+            nowMinute.text = String(minute)
+        }
+        
+        dateFormatter.dateFormat = "HH"
+        let Hour = 24 - Int(dateFormatter.string(from: todayTime))!
+        if Hour < 10 {
+            nowHour.text = "0"+String(Hour)
+        }else{
+            nowHour.text = String(Hour)
+        }
+        
         startTimer()
     }
     
     func startTimer(){
-        if let timer = mTimer {
-                    if !timer.isValid {
-                        mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeCallback), userInfo: nil, repeats: true)
+        if let secondTimer = mTimer {
+                    if !secondTimer.isValid {
+                        mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(secondCallback), userInfo: nil, repeats: true)
                     }
                 }else{
-                    mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeCallback), userInfo: nil, repeats: true)
+                    mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(secondCallback), userInfo: nil, repeats: true)
                 }
 
     }
     
-    @objc func timeCallback(){
+    @objc func secondCallback(){
+        makeCATransitionLabel(nowSecond)
         let todayTime = Date()
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH : mm : ss"
-        makeCATransitionLabel(nowTime)
-        nowTime.text = dateFormatter.string(from: todayTime)
+        dateFormatter.dateFormat = "ss"
+        let second = 60 - Int(dateFormatter.string(from: todayTime))!
+        
+        if second == 60{
+            makeCATransitionLabel(nowMinute)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "mm"
+            let minute = 59 - Int(dateFormatter.string(from: todayTime))!
+            
+            if minute == 59{
+                makeCATransitionLabel(nowHour)
+                dateFormatter.dateFormat = "HH"
+                let Hour = 24 - Int(dateFormatter.string(from: todayTime))!
+                if Hour < 10 {
+                    nowHour.text = "0"+String(Hour)
+                }else{
+                    nowHour.text = String(Hour)
+                }
+            }
+            if minute < 10 {
+                nowMinute.text = "0"+String(minute)
+            }else{
+                nowMinute.text = String(minute)
+            }
+        }
+        if second < 10 {
+            nowSecond.text = "0"+String(second)
+        }else{
+            nowSecond.text = String(second)
+        }
     }
     
     func makeCATransitionLabel(_ label: UILabel) {
@@ -47,12 +99,12 @@ class ViewController: UIViewController {
         transition.duration = 0.8
         transition.type = .push
         transition.subtype = .fromTop
-        nowTime.layer.add(transition, forKey: CATransitionType.push.rawValue)
+        label.layer.add(transition, forKey: CATransitionType.push.rawValue)
         
     }
     
     @IBAction func touchUpStartNotificationButton(){
-        
+        UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
     }
 
 
